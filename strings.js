572c0da27dfcfe495531807799b32778edc99392
etrapanob <jibonaparte@yahoo.com>
2021-03-24 20:43:29 -0400

    Strings Lab

---
+++ strings.js
@@ -0,0 +1,18 @@
+//Part 1:
+const x = 3;
+const y = 5;
+
+const mathString = "The value of " + x + " + " + y + " = " + (x + y);
+// const labSolution1 is my response to the TODO of Part 1
+const labSolution1 = `The value of ${x} and ${y} is ${x + y}.`;
+console.log(labSolution1);
+
+//Part 2:
+const name = "Homer D. Poe";
+const age = 40;
+
+const greeting = "Hello, my name is " + name + "!\nI am " + age + " years old.";
+
+// const labSolution2 is my response to the TODO of Part 2
+const labSolution2 = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(labSolution2);
